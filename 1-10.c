01f87522dcac4d4d4226b02c5fba11533620b749
darthvade <darthvade@sina.com>
2014-01-10 22:57:51 +0800

    1-10

--- 1-10.c
+++ 1-10.c
@@ -7,7 +7,7 @@ void straight_insert_Sort(int a[], const int start, const int end);
 //折半插入排序
 void binary_insert_Sort(int a[], const int start, const int end);
 //希尔排序
-void hill_Sort(int a[], const int start, const int end);
+void shell_Sort(int a[], const int start, const int end);
 //快速排序（非递归）
 void quick_Sort(int a[], const int start, const int end);
 
@@ -16,7 +16,7 @@ void display(int a[], int n);
 int main(){
 	int a[11] = {0, 9, 8, 6, 4, 1, 10, 19, 16, 5, 7};
 	display(a, 11);
-	straight_insert_Sort(a, 1, 10);
+	shell_Sort(a, 1, 10);
 	display(a, 11);
 return 0;
 }
@@ -25,7 +25,7 @@ void straight_insert_Sort(int a[], const int start, const int end){
 	int i, j;
 	for(i = start + 1; i <= end; i++){
 		a[0] = a[i];
-		for(j = i - 1; j >= start, a[j] > a[0]; j--){
+		for(j = i - 1; j >= start && a[j] > a[0]; j--){
 				a[j + 1] = a[j];
 		}
 		a[j + 1] = a[0];
@@ -33,11 +33,37 @@ void straight_insert_Sort(int a[], const int start, const int end){
 }
 
 void binary_insert_Sort(int a[], const int start, const int end){
-
+	int i, j, mid, m, n;
+	for(i = start + 1; i <= end; i++){
+		a[0] = a[i];
+		m = start; n = i;
+		while(m <= n){
+			mid = (m + n) / 2;
+			if(a[mid] > a[0]){
+				n = mid - 1;
+			}
+			else{
+				m = mid + 1;
+			}
+		}
+		for(j = i; j > m; j--){
+			a[j] = a[j - 1];
+		}
+		a[j] = a[0];
+	}
 }
-
-void hill_Sort(int a[], const int start, const int end){
-
+//有BUG！！！！！
+void shell_Sort(int a[], const int start, const int end){
+	int step, i, j;
+	for(step = (end - start) / 2; step >= 1; step = step / 2){
+		for(i = start + 1 + step; i <= end; i++){
+			a[0] = a[i];
+			for(j = i - 1 - step; j >= start && a[j] > a[0]; j = j - step){
+				a[j + 1 + step] = a[j];
+			}
+			a[j + step + 1] = a[0];
+		}
+	}	
 }
 
 void quick_Sort(int a[], const int start, const int end){
