ca30d4b30367825963324310f2c7813d3165b600
darthvade <darthvade@sina.com>
2014-04-10 23:42:56 +0800

    4-10

--- Week_6/4-9/systemIO/test.c
+++ Week_6/4-9/systemIO/test.c
@@ -8,5 +8,17 @@ int main() {
 
 	printf("\n%d\n", sizeof(input));
 
+	int fd = open("input.dat", O_RDONLY);
+
+	ssize_t n;
+	n = rio_readn(fd, input, sizeof(input));
+
+	printf("%d\n", n);
+	printf("%s\n", input);
+
+	char input2[] = {'a', 'b', '\0', 'c', 'd', '\0', 'd', '\0'};
+
+	printf("%s\n", input2);
+
 	return 0;
 }
