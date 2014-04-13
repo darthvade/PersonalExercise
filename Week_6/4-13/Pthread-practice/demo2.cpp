6be5ce3dbdd8ebb7f7b9ad593abb131a367bf697
darthvade <darthvade@sina.com>
2014-04-13 11:36:01 +0800

    2014-04-13 11:36:01 git-auto-saved

--- Week_6/4-13/Pthread-practice/demo2.cpp
+++ Week_6/4-13/Pthread-practice/demo2.cpp
@@ -9,6 +9,7 @@ using namespace std;
 pthread_mutex_t mutex;
 
 void *func(int *t) {
+	pthread_mutex_lock(&mutex);
 	cout << endl;
 	cout << "func started" << endl;
 	cout << *t << endl;
@@ -16,6 +17,7 @@ void *func(int *t) {
 	cout << *t << endl;
 	cout << "func finished" << endl;
 	cout << endl;
+	pthread_mutex_unlock(&mutex);
 	return (void *)0;
 }
 
@@ -28,9 +30,7 @@ int main() {
 
 	//对不符合回调函数类型的进行强制类型转换
 	for(int i = 0; i != NUM_THREAD; ++i) {
-//		pthread_mutex_lock(&mutex);
 		pthread_create(thread + i, NULL, (void *(*)(void *))func, &arg);
-//		pthread_mutex_unlock(&mutex);
 	}
 
 	pthread_mutex_destroy(&mutex);
