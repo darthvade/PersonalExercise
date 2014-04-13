685ae20885eb788931811d5c1b39b2b602dfd850
darthvade <darthvade@sina.com>
2014-04-13 22:30:01 +0800

    2014-04-13 22:30:01 git-auto-saved

--- Week_6/4-13/cpp/demo4.cpp
+++ Week_6/4-13/cpp/demo4.cpp
@@ -1,9 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
 class X {
 	private:
 		int data;
 	public:
 		void setData(int &input) const{
 //			this->data = input; // error!
+			cout << input << endl;
 		}
 		void setData(int &input) {
 			this->data = input; // right!
