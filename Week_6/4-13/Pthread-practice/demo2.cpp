18a9aab8f09afec994670d2799b4e40523a612a1
darthvade <darthvade@sina.com>
2014-04-13 11:31:01 +0800

    2014-04-13 11:31:01 git-auto-saved

--- Week_6/4-13/Pthread-practice/demo2.cpp
+++ Week_6/4-13/Pthread-practice/demo2.cpp
@@ -27,8 +27,9 @@ int main() {
 
 	//对不符合回调函数类型的进行强制类型转换
 	for(int i = 0; i != NUM_THREAD; ++i) {
-		pthread_lock(&mutex);
+		pthread_mutex_lock(&mutex);
 		pthread_create(thread + i, NULL, (void *(*)(void *))func, &arg);
+		pthread_mutex_unlock(&mutex);
 	}
 
 	for(int i = 0; i != NUM_THREAD; ++i) {
