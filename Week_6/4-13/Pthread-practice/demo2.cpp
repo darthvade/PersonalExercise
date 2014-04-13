2e3a3c6a24d4791db9da5a9111bc9a0c337df387
darthvade <darthvade@sina.com>
2014-04-13 11:18:01 +0800

    2014-04-13 11:18:01 git-auto-saved

--- Week_6/4-13/Pthread-practice/demo2.cpp
+++ Week_6/4-13/Pthread-practice/demo2.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+#define NUM_THREAD 5
+
 void *func(int *t) {
 	*t += 1;
 	cout << *t << endl;
@@ -12,8 +14,9 @@ void *func(int *t) {
 
 int main() {
 
-	pthread_t thread;
+	pthread_t thread[NUM_THREAD];
 	int arg = 100;
+	//对不符合回调函数类型的进行强制类型转换
 	pthread_create(&thread, NULL, (void *(*)(void *))func, &arg);
 
 	pthread_join(thread, NULL);
