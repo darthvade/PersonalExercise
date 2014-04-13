bb04985b1771a54cfa6f2e9e8e0a86c7e724dfb2
darthvade <darthvade@sina.com>
2014-04-13 10:21:01 +0800

    2014-04-13 10:21:01 git-auto-saved

--- Week_6/4-13/Pthread-practice/demo.cpp
+++ Week_6/4-13/Pthread-practice/demo.cpp
@@ -5,15 +5,13 @@
 using namespace std;
 
 void *func(void *t) {
-//	sleep(3);
 	cout << *((string *)t) << endl;
 	sleep(3);
 	return (void *)0;
 }
 
 void func2() {
-//	sleep(3);
-	cout << *((string *)t) << endl;
+	cout << "once func" << endl;
 	sleep(3);
 	return ;
 }
@@ -21,14 +19,13 @@ void func2() {
 int main() {
 	pthread_t thread;
 
-	string s1 = "child thread 1 ! once";
-	string s2 = "child thread 2 !";
+	string s1 = "child thread 1 !";
 
 	pthread_once_t one_contr = PTHREAD_ONCE_INIT;
 
 	pthread_once(&one_contr, func2);
 
-	pthread_create(&thread, NULL, func, &s2);
+	pthread_create(&thread, NULL, func, &s1);
 
 	pthread_join(thread, NULL);
 	cout << "Prog finished!" << endl;
