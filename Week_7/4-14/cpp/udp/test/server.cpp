977fb41f3896fa31d2dfcb078d7463484fc8fd9f
darthvade <darthvade@sina.com>
2014-04-14 21:13:01 +0800

    2014-04-14 21:13:01 git-auto-saved

--- Week_7/4-14/cpp/udp/test/server.cpp
+++ Week_7/4-14/cpp/udp/test/server.cpp
@@ -15,5 +15,9 @@ int main(int argc, char *argv[]) {
 	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
 	bzero(&servaddr, sizeof(servaddr));
+
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = hrons((unsigned short)argv[1]);
 	return 0;
 }
