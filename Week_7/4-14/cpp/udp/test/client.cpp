d22a10792a7a31b8e0c95f88903362bc3526e4f5
darthvade <darthvade@sina.com>
2014-04-14 22:47:01 +0800

    2014-04-14 22:47:01 git-auto-saved

--- Week_7/4-14/cpp/udp/test/client.cpp
+++ Week_7/4-14/cpp/udp/test/client.cpp
@@ -22,7 +22,8 @@ int main(int argc, char *argv[]) {
 	bzero(&servaddr, sizeof(servaddr));
 
 	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons((unsigned int)argv[2]);
+	//servaddr.sin_port = htons((unsigned int)argv[2]);
+	servaddr.sin_port = htons(argv[2]);
 	inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
 
 	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
