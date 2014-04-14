43e80e68faf607fda6357f694f24d58fe0026885
darthvade <darthvade@sina.com>
2014-04-14 17:22:01 +0800

    2014-04-14 17:22:01 git-auto-saved

--- Week_7/4-14/cpp/cond_mutex/Thread.h
+++ Week_7/4-14/cpp/cond_mutex/Thread.h
@@ -2,16 +2,18 @@
 #define __THREAD_H__
 
 #include <pthread.h>
+#include <string>
 
 class Thread {
 	public:
-		Thread(int num);
+		Thread(int num, std::string s);
 		~Thread();
 		void start();
 		void join();
 		void run();
 	private:
 		int num;
+		std::string s;
 		pthread_t thread;
 		static void* func(void *args);
 };
