e486b26f41e7a1b26e40b7182e45ecdb21c38571
darthvade <darthvade@sina.com>
2014-04-14 14:37:02 +0800

    2014-04-14 14:37:01 git-auto-saved

--- Week_7/4-14/cpp/cond_mutex/CondVar.h
+++ Week_7/4-14/cpp/cond_mutex/CondVar.h
@@ -2,11 +2,15 @@
 #define __CONDVAR_H__
 
 #include <pthread.h>
+#include "mutex.h"
 
 class CondVar{
 	public:
 		CondVar();
 		~CondVar();
+		void wait();
+		void notify();
+		void notify_all();
 	private:
 		pthread_cond_t condvar;
 };
