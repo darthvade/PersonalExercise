8bd414f28c3f0b264dafe8ce28629a1162c537e2
darthvade <darthvade@sina.com>
2014-04-14 21:34:02 +0800

    2014-04-14 21:34:01 git-auto-saved

--- Week_7/4-14/cpp/udp/test/client.cpp
+++ Week_7/4-14/cpp/udp/test/client.cpp
@@ -9,8 +9,13 @@ int main(int argc, char *argv[]) {
 	if(argc != 3) {
 		cerr << "Error! [IPADDR] [PORT]" << endl;
 	}
+
 	int sockfd;
 	struct sockaddr_t servaddr;
+	
+	bzero(&servaddr, sizeof(servaddr));
+
+	servaddr.
 
 	return 0;
 }
