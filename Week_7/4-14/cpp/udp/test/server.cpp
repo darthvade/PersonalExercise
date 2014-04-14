9ed39a5df3ab02938abf05d596b0991ee3eca342
darthvade <darthvade@sina.com>
2014-04-14 23:00:01 +0800

    2014-04-14 23:00:01 git-auto-saved

--- Week_7/4-14/cpp/udp/test/server.cpp
+++ Week_7/4-14/cpp/udp/test/server.cpp
@@ -39,7 +39,7 @@ int main(int argc, char *argv[]) {
 		n = recvfrom(sockfd, msg, 1024, 0, (SA)&cliaddr, &len);
 		string s(msg);
 		s.reserve(s.size());
-		sendto(sockfd, msg, n, 0, (SA)&cliaddr, len);
+		sendto(sockfd, s.c_str(), n, 0, (SA)&cliaddr, len);
 	}
 	
 
