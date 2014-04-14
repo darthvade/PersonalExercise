38fbdb3d211df067000e665c42ed2f00a9a0a007
darthvade <darthvade@sina.com>
2014-04-14 21:45:01 +0800

    2014-04-14 21:45:01 git-auto-saved

---
+++ Week_7/4-14/cpp/udp/test/client.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <cstring>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	if(argc != 3) {
+		cerr << "Error! [IPADDR] [PORT]" << endl;
+	}
+
+	int sockfd;
+	struct sockaddr_in servaddr;
+	
+	bzero(&servaddr, sizeof(servaddr));
+
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons((unsigned int)argv[2]);
+	inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	int n;
+	char sentMsg[1024], getMsg[1024];
+	while(fgets(sentMsg, 1024, stdin) != NULL) {
+	
+	}
+
+	return 0;
+}
