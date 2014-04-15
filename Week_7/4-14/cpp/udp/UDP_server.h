4e6619d3f339c96372dee5670552610946c556e6
darthvade <darthvade@sina.com>
2014-04-15 10:13:01 +0800

    2014-04-15 10:13:01 git-auto-saved

---
+++ Week_7/4-14/cpp/udp/UDP_server.h
@@ -0,0 +1,15 @@
+#ifndef __UDP_SERVER_H__
+#define __UDP_SERVER_H__
+
+#include <iostream>
+#include <cstdio>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <cstring>
+#include <cstdlib>
+
+using namespace std;
+
+#endif
