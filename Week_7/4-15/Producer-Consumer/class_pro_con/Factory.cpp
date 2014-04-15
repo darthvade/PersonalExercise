a8e6a5b67ed08bf132a0f058449516e9ceeed333
darthvade <darthvade@sina.com>
2014-04-15 20:34:01 +0800

    2014-04-15 20:34:01 git-auto-saved

--- Week_7/4-15/Producer-Consumer/class_pro_con/Factory.cpp
+++ Week_7/4-15/Producer-Consumer/class_pro_con/Factory.cpp
@@ -5,7 +5,9 @@ Factory::Factory(int num_consumer,
 				int num_producer,
 				int fre_producer) : 
 	_num_consumer(num_consumer), 
-	_fre_consumer()
+	_fre_consumer(fre_consumer),
+	_num_producer(num_producer),
+	_fre_producer(fre_producer)
 	{
 
 }
