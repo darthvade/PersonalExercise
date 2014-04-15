0e13b0a3c11732e622fbb8216c8bea3c4f53ee54
darthvade <darthvade@sina.com>
2014-04-15 17:18:02 +0800

    2014-04-15 17:18:02 git-auto-saved

--- Week_7/4-15/Producer-Consumer/class_pro_con/ProducerThread.cpp
+++ Week_7/4-15/Producer-Consumer/class_pro_con/ProducerThread.cpp
@@ -10,15 +10,15 @@ ProducerThread::~ProducerThread() {
 }
 
 void ProducerThread::start() {
-
+	pthread_create(&_p_id, NULL, _producer_action, this);
 }
 
 void ProducerThread::join() {
-
+	pthread_join(_p_id, NULL);
 }
 
 void* ProducerThread::_producer_action(void *args) {
-
+	ProducerThread *pproducer = static_cast<ProducerThread *>(args);
 	pthread_exit((void *)0);
 }
 
