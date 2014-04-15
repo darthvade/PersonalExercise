7f8ad7c4322d5285f0145dd518d29dcda1b03f46
darthvade <darthvade@sina.com>
2014-04-15 16:34:02 +0800

    2014-04-15 16:34:01 git-auto-saved

--- Week_7/4-15/Producer-Consumer/class_pro_con/ProducerThread.cpp
+++ Week_7/4-15/Producer-Consumer/class_pro_con/ProducerThread.cpp
@@ -7,3 +7,15 @@ ProducerThread::ProducerThread() {
 ProducerThread::~ProducerThread() {
 
 }
+
+void ProducerThread::start() {
+
+}
+
+void ProducerThread::join() {
+
+}
+
+void ProducerThread::_producer_action() {
+
+}
