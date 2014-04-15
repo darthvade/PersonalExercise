2a933498dc0ee3248e442989d475a679c7ff740e
darthvade <darthvade@sina.com>
2014-04-15 14:41:01 +0800

    2014-04-15 14:41:01 git-auto-saved

--- Week_7/4-15/Producer-Consumer/pro_con.cpp
+++ Week_7/4-15/Producer-Consumer/pro_con.cpp
@@ -10,30 +10,32 @@ pthread_mutex_t gmutex;
 
 pthread_cond_t gcond;
 
-void *producer_func(void *args) {
+void *producer_func(void *) {
 	while(1) {
- 		 pthread_mutex_lock(&gmutex);
- 		 
- 		 Q.push(1);
- 		 cout << "Push a num" << endl;
- 		 pthread_cond_signal(&gcond);
+ 		pthread_mutex_lock(&gmutex);
+ 			 
+ 		Q.push(1);
+ 		cout << "Push a num" << endl;
+ 		pthread_cond_signal(&gcond);
  
- 		 pthread_mutex_unlock(&gmutex);
+ 		pthread_mutex_unlock(&gmutex);
 	}
 	pthread_exit((void *)0);
 }
 
-void *consumer_func(void *args) {
-	pthread_mutex_lock(&gmutex);
+void *consumer_func(void *) {
+	while(1){
+		pthread_mutex_lock(&gmutex);
 
-	while(Q.empty()) {
-		pthread_cond_wait(&gcond, &gmutex);
+		while(Q.empty()) {
+			pthread_cond_wait(&gcond, &gmutex);
+		}
+		int num = Q.front();
+		Q.pop();
+		cout << "get a num " << num << endl;
+
+		pthread_mutex_unlock(&gmutex);
 	}
-	int num = Q.front();
-	Q.pop();
-	cout << "get a num " << num << endl;
-
-	pthread_mutex_unlock(&gmutex);
 	pthread_exit((void *)0);
 }
 
