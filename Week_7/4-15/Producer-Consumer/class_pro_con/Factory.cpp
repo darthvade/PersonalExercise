f084cc82520d757783b715b6470043c9b0181052
darthvade <darthvade@sina.com>
2014-04-15 21:21:01 +0800

    2014-04-15 21:21:01 git-auto-saved

--- Week_7/4-15/Producer-Consumer/class_pro_con/Factory.cpp
+++ Week_7/4-15/Producer-Consumer/class_pro_con/Factory.cpp
@@ -1,14 +1,8 @@
 #include "Factory.h"
 
-Factory::Factory(
-	std::size_t num_consumer,
-	std::size_t fre_consumer,
-	std::size_t num_producer,
-	std::size_t fre_producer ) : 
-	_num_consumer(num_consumer), 
-	_fre_consumer(fre_consumer),
-	_num_producer(num_producer),
-	_fre_producer(fre_producer) {
+Factory::Factory( std::size_t num_consumer, std::size_t fre_consumer, std::size_t num_producer, std::size_t fre_producer ) : 
+				  _num_consumer(num_consumer), _fre_consumer(fre_consumer),
+				  _num_producer(num_producer), _fre_producer(fre_producer) {
 	for(std::size_t i = 0; i != _num_producer; ++i) {
 		ProducerThread temp(&_wq, fre_producer, &_cout_lock);
 		ProducerThread &p = temp;
