82b860b96455b8e01fa276bc3016c342a8cc6fb3
darthvade <darthvade@sina.com>
2014-04-15 20:52:01 +0800

    2014-04-15 20:52:01 git-auto-saved

--- Week_7/4-15/Producer-Consumer/class_pro_con/Factory.cpp
+++ Week_7/4-15/Producer-Consumer/class_pro_con/Factory.cpp
@@ -9,7 +9,7 @@ Factory::Factory(
 	_fre_consumer(fre_consumer),
 	_num_producer(num_producer),
 	_fre_producer(fre_producer) {
-	for(int i = 0; i != _num_producer; ++i) {
+	for(std::size_t i = 0; i != _num_producer; ++i) {
 		ProducerThread(&_wq, fre_producer, &_cout_lock);
 	}	
 	for(int i = 0; i != _num_consumer; ++i) {
