64d0f616217225b61e2f7fb862dccafe6df34c81
darthvade <darthvade@sina.com>
2014-04-15 17:42:01 +0800

    2014-04-15 17:42:01 git-auto-saved

--- Week_7/4-15/Producer-Consumer/class_pro_con/demo.cpp
+++ Week_7/4-15/Producer-Consumer/class_pro_con/demo.cpp
@@ -6,9 +6,13 @@ int main() {
 	WorkingQueue wq;
 
 	ProducerThread pro_t(&wq);
+	ProducerThread pro_t2(&wq);
+	ProducerThread pro_t3(&wq);
 	ConsumerThread con_t(&wq);
 
 	pro_t.start();
+	pro_t2.start();
+	pro_t3.start();
 	con_t.start();
 
 	pro_t.join();
