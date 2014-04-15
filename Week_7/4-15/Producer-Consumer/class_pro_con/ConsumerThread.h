adeb5e619e14ef3a703c571f569fd95ebe05bb15
darthvade <darthvade@sina.com>
2014-04-15 16:32:01 +0800

    2014-04-15 16:32:01 git-auto-saved

--- Week_7/4-15/Producer-Consumer/class_pro_con/ConsumerThread.h
+++ Week_7/4-15/Producer-Consumer/class_pro_con/ConsumerThread.h
@@ -15,5 +15,4 @@ class ConsumerThread {
 		void _run();
 };
 
-
 #endif
--- Week_7/4-15/Producer-Consumer/class_pro_con/ProducerThread.h
+++ Week_7/4-15/Producer-Consumer/class_pro_con/ProducerThread.h
@@ -5,6 +5,10 @@
 
 class ProducerThread {
 	public:
+		ProducerThread();
+		~ProducerThread();
+		void start();
+		void join();
 	private:
 		pthread_t _p_id;
 		static void _producer_action();
