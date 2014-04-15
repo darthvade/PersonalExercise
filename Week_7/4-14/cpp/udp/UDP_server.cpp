c7af6931d57796d9ce77fc5334398c1705094b6f
darthvade <darthvade@sina.com>
2014-04-15 10:34:01 +0800

    2014-04-15 10:34:01 git-auto-saved

--- Week_7/4-14/cpp/udp/UDP_server.cpp
+++ Week_7/4-14/cpp/udp/UDP_server.cpp
@@ -2,11 +2,11 @@
 
 UDPServer::UDPServer(int port): _servport(port) {
 	_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-	_bind(sockfd, (SA *)&_servaddr, sizeof(_servaddr));
+	_bind();
 }
 
 UDPServer::~UDPServer() {
-
+	close(_sockfd);
 }
 
 std::string UDPServer::receive() {
@@ -23,5 +23,5 @@ void UDPServer::_bind() {
 	_servaddr.sin_family = AF_INET;
 	_servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	_servaddr.sin_port = htons(_servport);
-	bind(sockfd, (SA *)&_servaddr, sizeof(_servaddr));
+	bind(_sockfd, (SA *)&_servaddr, sizeof(_servaddr));
 }
