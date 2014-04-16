2cd5663f2f6ef6e807df7ba16db9aef563d422cb
darthvade <darthvade@sina.com>
2014-04-16 15:25:01 +0800

    2014-04-16 15:25:01 git-auto-saved

---
+++ Week_7/4-16/cpp/vector_text3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+	vector<int> vec;
+	vec.push_back(1);
+	vec.push_back(2);
+	vec.push_back(3);
+	vec.push_back(4);
+	vec.push_back(5);
+	
+	for(vector<int>::const_iterator iter = vec.begin(); iter != vec.end(); ++iter) {
+	
+	}
+
+	return 0;
+}
