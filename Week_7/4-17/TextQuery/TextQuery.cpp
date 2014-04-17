300b6de644d90c0124dbcb08d23b22905e769c70
darthvade <darthvade@sina.com>
2014-04-17 19:44:02 +0800

    2014-04-17 19:44:02 git-auto-saved

--- Week_7/4-17/TextQuery/TextQuery.cpp
+++ Week_7/4-17/TextQuery/TextQuery.cpp
@@ -23,4 +23,7 @@ void TextQuery::build_map() {
 set<TextQuery::line_no>
 TextQuery::run_query(const std::string query_word) const {
 	map< string, set<line_no> >::const_iterator iter = word_map.find(query_word);
+	if(iter == word_map.end()) {
+		return set<line_no>();
+	}
 }
