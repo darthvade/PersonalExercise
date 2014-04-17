440e9bf4b43e4cb2e46c39f70ae7d686ad2310eb
darthvade <darthvade@sina.com>
2014-04-17 18:59:01 +0800

    2014-04-17 18:59:01 git-auto-saved

---
+++ Week_7/4-17/TextQuary/TextQuery.h
@@ -0,0 +1,15 @@
+#ifndef __TEXTQUERY_H__
+#define __TEXTQUERY_H__
+
+#include <vector>
+#include <set>
+#include <string>
+#include <map>
+
+class TextQuery {
+public:
+	typedef std::
+};
+
+
+#endif
