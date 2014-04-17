15f6b90017e4f2b96e03afacd96ecd908c53e418
darthvade <darthvade@sina.com>
2014-04-17 15:13:01 +0800

    2014-04-17 15:13:01 git-auto-saved

--- Week_7/4-17/test_set.cpp
+++ Week_7/4-17/test_set.cpp
@@ -9,6 +9,7 @@ int main() {
 	vector<int> vect;
 	for(vector<int>::size_type ix = 0; ix != 10; ++ix) {
 		vect.push_back(ix);
+		vect.push_back(ix);
 	}
 	set<int> myset(vect.begin(), vect.end());
 	cout << "vect " << vect.size() << endl;  
