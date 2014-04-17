6e1c4a8aa6cc8eb412f9865909549ee1807717c4
darthvade <darthvade@sina.com>
2014-04-17 19:28:01 +0800

    2014-04-17 19:28:01 git-auto-saved

--- Week_7/4-17/TextQuery/TextQuery.h
+++ Week_7/4-17/TextQuery/TextQuery.h
@@ -16,7 +16,7 @@ public:
 	std::set<line_no> run_query(const std::string) const;
 	std::string text_line(line_no) const;
 private:
-	void store_file(std::ifstream&);
+	void store_file(std::ifstream& is);
 	void build_map();
 	std::vector<std::string> lines_of_text;
 	std::map< std::string, std::set<line_no> > word_map;
