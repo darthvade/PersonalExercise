5a41dc64ed12968b2b5ecd2491d3a51451bb3294
darthvade <darthvade@sina.com>
2014-04-17 19:15:01 +0800

    2014-04-17 19:15:01 git-auto-saved

--- Week_7/4-17/TextQuery/TextQuery.h
+++ Week_7/4-17/TextQuery/TextQuery.h
@@ -9,14 +9,15 @@
 class TextQuery {
 public:
 	typedef std::vector<std::string>::size_type line_no;
-	void read_file(std::string &is) {
+	void read_file(std::ifstream &is) {
 		store_file(is);
 		build_map();
 	}
 	std::set<line_no> run_query(const std::string) const;
 	std::string text_line(line_no) const;
 private:
-	void store_file(std::string);
+	void store_file(std::ifstream&);
+	
 };
 
 
