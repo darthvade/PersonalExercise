4fbccf862de44bb3f53942b1a93d31edd920b656
darthvade <darthvade@sina.com>
2014-04-17 19:27:01 +0800

    2014-04-17 19:27:01 git-auto-saved

--- Week_7/4-17/TextQuery/TextQuery.cpp
+++ Week_7/4-17/TextQuery/TextQuery.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 void TextQuery::store_file(std::ifstream &is) {
-	string textline;
+	std::string textline;
 	while(getline(is, textline)) {
 		lines_of_text.push_back(textline);
 	}
