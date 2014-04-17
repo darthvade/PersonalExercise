f86343dac7021a1046dde09d8267912e0f394611
darthvade <darthvade@sina.com>
2014-04-17 15:11:02 +0800

    2014-04-17 15:11:02 git-auto-saved

--- Week_7/4-17/text_set.cpp
+++ Week_7/4-17/text_set.cpp
@@ -10,6 +10,9 @@ int main() {
 	for(vector<int>::size_type ix = 0; ix != 10; ++ix) {
 		vect.push_back(ix);
 	}
+	set<int> myset(vect.begin(), vect.end());
+	cout << "vect " << vect.size() << endl;  
+	cout << "myset " << myset.size() << endl;  
 
 	return 0;
 }
