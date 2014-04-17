e5521ff21cda70776932226eeb4ef841335e73de
darthvade <darthvade@sina.com>
2014-04-17 19:53:01 +0800

    2014-04-17 19:53:01 git-auto-saved

--- Week_7/4-17/TextQuery/main.cpp
+++ Week_7/4-17/TextQuery/main.cpp
@@ -1,9 +1,12 @@
 #include "TextQuery.h"
 #include <fstream>
 
+using namespace std;
+
 int main() {
 	ifstream infile;
 	if(argc < 2 || !open_file(infile, argv[1])) {
-		cerr << "" << endl;
+		cerr << "No input file!" << endl;
+		return EXIT_FAILURE;
 	}
 }
