345f1ea39c7d7d2c722a47eb6b342818a827ac63
darthvade <darthvade@sina.com>
2014-04-17 12:33:05 +0800

    2014-04-17 12:33:02 git-auto-saved

--- Week_7/4-17/word_count2.cpp
+++ Week_7/4-17/word_count2.cpp
@@ -11,6 +11,9 @@ int main() {
 	string temp;
 	while(fs >> temp) {
 		pair<map<string, int>::iterator , bool> ret = counter.insert(make_pair(temp, 1));
+		if(!ret.second) {
+			++ret.first->second;
+		}
 	}
 	for(map<string, int>::const_iterator iter = counter.begin(); iter != counter.end(); ++iter) {
 		cout << iter->first << " " << iter->second << endl;
