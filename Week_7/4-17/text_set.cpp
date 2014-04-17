e4535b00aa7785bed2b0dd0f2ff6fbba22764acd
darthvade <darthvade@sina.com>
2014-04-17 15:10:01 +0800

    2014-04-17 15:10:01 git-auto-saved

---
+++ Week_7/4-17/text_set.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	
+	vector<int> vect;
+	for(vector<int>::size_type ix = 0; ix != 10; ++ix) {
+		vect.push_back(ix);
+	}
+
+	return 0;
+}
