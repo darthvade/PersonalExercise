9b723ff62d9221cfc48c776e97de0e9b0b6b3c7b
darthvade <darthvade@sina.com>
2014-04-17 19:18:01 +0800

    2014-04-17 19:18:01 git-auto-saved

--- Week_7/4-17/TextQuery/TextQuery.h
+++ Week_7/4-17/TextQuery/TextQuery.h
@@ -18,7 +18,7 @@ public:
 private:
 	void store_file(std::ifstream&);
 	void build_map();
-	std::vector<std::string> line_of_text;
+	std::vector<std::string> lines_of_text;
 	std::map< std::string, std::set<line_no> > word_map;
 };
 
