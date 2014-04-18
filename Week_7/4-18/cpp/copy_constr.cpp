aded215752ece87720e8c5530c14eae9f2aaac21
darthvade <darthvade@sina.com>
2014-04-18 11:16:01 +0800

    2014-04-18 11:16:01 git-auto-saved

---
+++ Week_7/4-18/cpp/copy_constr.cpp
@@ -0,0 +1,18 @@
+#include <string.h>
+#include <iostream>
+
+using namespace std;
+
+class String {
+public:
+	String() {
+		_pstr = new char[10];
+	}
+private:
+	char *_pstr;
+};
+
+int main() {
+
+	return 0;
+}
