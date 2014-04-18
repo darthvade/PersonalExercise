e54573f137f7df3341e43fcba119f0c1d2492b84
darthvade <darthvade@sina.com>
2014-04-18 11:22:01 +0800

    2014-04-18 11:22:01 git-auto-saved

--- Week_7/4-18/cpp/copy_constr.cpp
+++ Week_7/4-18/cpp/copy_constr.cpp
@@ -8,6 +8,10 @@ public:
 	String() {
 		_pstr = new char[10];
 	}
+	String(const char *copy) {
+		_pstr = new char[strlen(copy) + 1];
+		strcpy(_pstr, copy);
+	}
 	String(const String& copy) {
 		_pstr = new char[strlen(copy._pstr) + 1];
 		strcpy(_pstr, copy._pstr);
@@ -20,6 +24,6 @@ private:
 };
 
 int main() {
-
+	String s("");
 	return 0;
 }
