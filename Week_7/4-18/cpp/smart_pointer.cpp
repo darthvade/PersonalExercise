fbf5c5ec5bf1e202026ca56977a02313ea328c8a
darthvade <darthvade@sina.com>
2014-04-18 20:25:01 +0800

    2014-04-18 20:25:01 git-auto-saved

--- Week_7/4-18/cpp/smart_pointer.cpp
+++ Week_7/4-18/cpp/smart_pointer.cpp
@@ -18,25 +18,13 @@ private:
 class Integer {
 public:
 	//默认构造函数
-	Integer():_iptr(new int(0)){}
 	//显式构造函数
-	Integer(const int i):_iptr(new int(i)) {}
 	//拷贝构造函数
-	Integer(const Integer &i) {
-		_iptr = i._iptr;
-	}
 	//复制运算符
-	Integer& operator=(const Integer &i) {
-		_iptr = i._iptr;	
-		return *this;
-	}
 	//析构函数
-	~Integer() {
-		delete _iptr;
-	}
 	//debug
 	void debug() {
-		cout << *_iptr << endl;
+		cout << *_iptr->_sptr << endl;
 	}
 private:
 	Smartptr *_iptr;
