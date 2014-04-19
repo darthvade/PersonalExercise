24c2c861e9601bc9ac2f30dadc042daedcb095c3
darthvade <darthvade@sina.com>
2014-04-19 10:55:01 +0800

    2014-04-19 10:55:01 git-auto-saved

--- Week_7/4-19/NetworkIO/client.cpp
+++ Week_7/4-19/NetworkIO/client.cpp
@@ -3,10 +3,12 @@
 #include <netinet/in.h>
 #include <iostream>
 
-int main() {
+int main(int argc, char *argv[]) {
 
 	int clientfd;
 	clientfd = socket(AF_INET, SOCK_STREAM, 0);
+
+
 
 	return 0;
 }
