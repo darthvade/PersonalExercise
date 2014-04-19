0146215e86f772c0931488cd204242b21c397582
darthvade <darthvade@sina.com>
2014-04-19 10:56:01 +0800

    2014-04-19 10:56:01 git-auto-saved

--- Week_7/4-19/NetworkIO/client.cpp
+++ Week_7/4-19/NetworkIO/client.cpp
@@ -3,12 +3,14 @@
 #include <netinet/in.h>
 #include <iostream>
 
+typedef struct sockaddr SA;
+
 int main(int argc, char *argv[]) {
 
 	int clientfd;
 	clientfd = socket(AF_INET, SOCK_STREAM, 0);
 
-
+	struct sockaddr_in seraddr;
 
 	return 0;
 }
