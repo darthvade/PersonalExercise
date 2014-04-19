042b362b09fa51eaed375537a5fc68e749181378
darthvade <darthvade@sina.com>
2014-04-19 10:06:01 +0800

    2014-04-19 10:06:01 git-auto-saved

--- Week_7/4-19/NetworkIO/server.cpp
+++ Week_7/4-19/NetworkIO/server.cpp
@@ -1,11 +1,18 @@
+/*
+ *
+ *
+ *
+ */
+
 #include <iostream>
 #include <sys/socket.h>
 
 int main(int argc, char *argv[]) {
 
 	int serverfd;
-
 	serverfd = socket(AF_INET, SOCK_STREAM, 0);
+
+
 
 	return 0;
 }
