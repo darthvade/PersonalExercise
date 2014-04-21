046acce7296bba5723a621521a2e253ee26d1c38
darthvade <darthvade@sina.com>
2014-04-21 09:47:01 +0800

    2014-04-21 09:47:01 git-auto-saved

--- Week_7/4-15/Producer-Consumer/pro_con.cpp
+++ Week_7/4-15/Producer-Consumer/pro_con.cpp
@@ -19,7 +19,6 @@ void *producer_func(void *) {
  		pthread_mutex_lock(&gmutex);
  			 
 		while(Q.size() == 5) {
-			pthread_cond_wait(&empty, &gmutex);
  			Q.push(1);
  			cout << "Push a num" << endl;
  			pthread_cond_signal(&full);
