bae8bfa461e93d93f0e1d9ffb617fad1076e0a29
darthvade <darthvade@sina.com>
2014-04-22 10:50:01 +0800

    2014-04-22 10:50:01 git-auto-saved

--- Week_8/4-22/cpp/derive2.cpp
+++ Week_8/4-22/cpp/derive2.cpp
@@ -8,11 +8,16 @@ public:
 	void print() { 
 		cout << "id: " << _id << " name: " << _name << endl;   
 	}	
+	void setinfo(int id, const string &s) {
+		_id = id;
+		_name = s;
+	}
 private:
 	int _id;
 	string _name;
 };
 
 class Student : public Person {
-
+private:
+	string school;
 };
