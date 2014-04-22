19fdce1b27ccd477cab240d363a65982b7686e0e
darthvade <darthvade@sina.com>
2014-04-22 10:46:01 +0800

    2014-04-22 10:46:01 git-auto-saved

--- Week_8/4-22/cpp/derive2.cpp
+++ Week_8/4-22/cpp/derive2.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 class Person {
 public:
-	Person(int id, const char *s) : _id(id), _name(s) {};
-	void print() 
+	Person(int id, const char *s) : _id(id), _name(s) {}
+	void print() { 
 		cout << "id: " << _id << " name: " << _name << endl;   
 	}	
 protected:
