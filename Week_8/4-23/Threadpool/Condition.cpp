15908ec91f750d76ed1c29248864e50000db79a8
darthvade <darthvade@sina.com>
2014-04-23 23:20:01 +0800

    2014-04-23 23:20:01 git-auto-saved

---
+++ Week_8/4-23/Threadpool/Condition.cpp
@@ -0,0 +1,21 @@
+#include "Condition.h"
+
+Condition::Condition() {
+
+}
+
+Condition::~Condition() {
+
+}
+
+void Condition::wait() {
+
+}
+
+void Condition::notify() {
+
+}
+
+void Condition::notifyall() {
+
+}
