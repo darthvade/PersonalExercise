2d7c6e3b459d61642ef0046ce79a31c9ac4f3820
darthvade <darthvade@sina.com>
2014-04-23 23:51:02 +0800

    2014-04-23 23:51:01 git-auto-saved

--- Week_8/4-23/Threadpool/Threadpool.h
+++ Week_8/4-23/Threadpool/Threadpool.h
@@ -1,8 +1,15 @@
 #ifndef __THREADPOOL_H__
 #define __THREADPOOL_H__
 
+#include <queue>
+#include <vector>
+#include "MutexLock.h"
+#include "Condition.h"
+#include "Noncopyable.h"
+
 struct Task {
 	int _num;
 };
+
 
 #endif
