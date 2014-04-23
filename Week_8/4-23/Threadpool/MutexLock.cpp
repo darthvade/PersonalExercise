1f6947b3136ff8a4abf041cdff058d4d89168153
darthvade <darthvade@sina.com>
2014-04-23 23:07:01 +0800

    2014-04-23 23:07:01 git-auto-saved

--- Week_8/4-23/Threadpool/MutexLock.cpp
+++ Week_8/4-23/Threadpool/MutexLock.cpp
@@ -2,8 +2,12 @@
 
 Mutex::Mutex() {
 	pthread_mutex_init(&_mutex, NULL);
-};
+}
 
 Mutex::~Mutex() {
+	pthread_mutex_destroy(&_mutex);
+}
 
-};
+void Mutex::lock() {
+	pthread_mutex_lock(&_mutex);
+}
