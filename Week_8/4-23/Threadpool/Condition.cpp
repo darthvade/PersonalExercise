092154548b19b1e4e79dbd83a2f8c3e4a8412445
darthvade <darthvade@sina.com>
2014-04-23 23:21:01 +0800

    2014-04-23 23:21:01 git-auto-saved

--- Week_8/4-23/Threadpool/Condition.cpp
+++ Week_8/4-23/Threadpool/Condition.cpp
@@ -1,11 +1,11 @@
 #include "Condition.h"
 
 Condition::Condition() {
-
+	pthread_cond_init(&_cond, NULL);
 }
 
 Condition::~Condition() {
-
+	pthread_cond_destroy(&_cond);
 }
 
 void Condition::wait() {
