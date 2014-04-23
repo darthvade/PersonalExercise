bd5acd012af344d0e8fc2bcb1678f88ed6b25876
darthvade <darthvade@sina.com>
2014-04-23 23:14:01 +0800

    2014-04-23 23:14:01 git-auto-saved

--- Week_8/4-23/Threadpool/Condition.h
+++ Week_8/4-23/Threadpool/Condition.h
@@ -3,12 +3,15 @@
 
 #include <pthread.h>
 #include "Noncopyable.h"
+#include "MutexLock.h"
 
 class Condition : public Noncopyable {
 public:
 	Condition();
 	~Condition();
-void wait();
+	void wait();
+	void notify();
+	void notifyall();
 private:
 	Mutex *_pMutex;
 	pthread_cond_t _cond;
