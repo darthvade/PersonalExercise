445898c13cf94aeb10fa08b86a6a27ff44657cbc
darthvade <darthvade@sina.com>
2014-04-24 00:30:01 +0800

    2014-04-24 00:30:01 git-auto-saved

--- Week_8/4-23/Threadpool/Threadpool.cpp
+++ Week_8/4-23/Threadpool/Threadpool.cpp
@@ -2,7 +2,9 @@
 
 Threadpool::Threadpool(std::vector<Workthread>::size_type max_thread) :
 	_task_queue(), _max_thread(max_thread), _thread_vector(_max_thread), _is_started(false), _lock(), _cond(&_lock){
-
+	for(std::vector<Workthread>::iterator iter = _thread_vector.begin(); iter != _thread_vector.end(); ++iter) {
+		
+	}
 }
 
 Threadpool::~Threadpool() {
