a01626e3eee5492bd88c6e9952adb73d02e764c4
darthvade <darthvade@sina.com>
2014-04-23 23:40:01 +0800

    2014-04-23 23:40:01 git-auto-saved

--- Week_8/4-23/Threadpool/Thread.cpp
+++ Week_8/4-23/Threadpool/Thread.cpp
@@ -9,11 +9,11 @@ Thread::~Thread() {
 }
 
 void Thread::start() {
-
+	pthread_create(&_tid, NULL, thread_func, this);
 }
 
 void Thread::join() {
-
+	pthread_join(&_tid, NULL);
 }
 
 void *Thread::thread_func(void *args) {
