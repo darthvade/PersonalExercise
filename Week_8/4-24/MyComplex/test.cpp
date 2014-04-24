59b201ed0c73081f4f1fa3589a6fdc6961578615
darthvade <darthvade@sina.com>
2014-04-24 10:54:01 +0800

    2014-04-24 10:54:01 git-auto-saved

--- Week_8/4-24/MyComplex/test.cpp
+++ Week_8/4-24/MyComplex/test.cpp
@@ -12,5 +12,30 @@ int main() {
 	cout << a * b << endl;
 	cout << (a == b) << endl;
 	*/
+	MyComplex complex_obj1 ;
+
+	MyComplex complex_obj2(3,4);
+
+	MyComplex complex_obj3 , complex_obj4 ;
+
+	cin >> complex_obj3 >> complex_obj4 ;
+
+	cout << complex_obj2 + complex_obj3 << endl ;
+
+	cout << complex_obj2 - complex_obj3 << endl ;
+
+	cout << complex_obj2 * complex_obj4 << endl ;
+
+	cout << (complex_obj2 == complex_obj3) << endl ;
+
+	MyComplex  complex_obj5(complex_obj2) ;
+
+	cout << complex_obj5  ;
+
+	MyComplex  complex_obj6 ;
+
+	complex_obj6 = complex_obj2 ;
+
+	cout << complex_obj6 << endl ;
 	return 0;
 }
