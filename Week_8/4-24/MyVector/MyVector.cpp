72dc2ab5ecfa1ce65ca3c3cb264ce98c6a46d022
darthvade <darthvade@sina.com>
2014-04-24 14:06:01 +0800

    2014-04-24 14:06:01 git-auto-saved

--- Week_8/4-24/MyVector/MyVector.cpp
+++ Week_8/4-24/MyVector/MyVector.cpp
@@ -17,7 +17,8 @@ MyVector::MyVector(int *begin, int *end) {
 		_size = n;
 		_capacity = n;
 		_myptr = new int[n];
-		memmove(_myptr, begin, n);
+		std::cout << sizeof(_myptr) << std::endl;
+		memmove(_myptr, begin,n);
 	}
 }
 
