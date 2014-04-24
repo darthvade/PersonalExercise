4c4b22765c62340b42d451bb660ef1842c805fba
darthvade <darthvade@sina.com>
2014-04-24 17:03:01 +0800

    2014-04-24 17:03:01 git-auto-saved

--- Week_8/4-24/cpp/producer_consumer.cpp
+++ Week_8/4-24/cpp/producer_consumer.cpp
@@ -7,6 +7,10 @@ pthread_cond_t empty;
 pthread_cond_t full;
 
 int main() {
+	pthread_mutex_init(&mutex, NULL);
+	pthread_cond_init(&empty, NULL);
+	pthread_cond_init(&full, NULL);
+
 
 	return 0;
 }
