1bf32ffd7cdb1f06be63482eba636e33f916e62b
darthvade <darthvade@sina.com>
2014-04-24 10:53:01 +0800

    2014-04-24 10:53:01 git-auto-saved

--- Week_8/4-24/MyComplex/test.cpp
+++ Week_8/4-24/MyComplex/test.cpp
@@ -4,12 +4,13 @@
 using namespace std;
 
 int main() {
+	/*
 	MyComplex a(3, 4);
 	MyComplex b(4, 5);
 	cout << a + b << endl;
 	cout << a - b << endl;
 	cout << a * b << endl;
 	cout << (a == b) << endl;
-
+	*/
 	return 0;
 }
