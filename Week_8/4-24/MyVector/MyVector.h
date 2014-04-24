73dc7db61afd4742ae20f9b9bdd9f60a5c52c3c8
darthvade <darthvade@sina.com>
2014-04-24 13:19:01 +0800

    2014-04-24 13:19:01 git-auto-saved

--- Week_8/4-24/MyVector/MyVector.h
+++ Week_8/4-24/MyVector/MyVector.h
@@ -10,6 +10,7 @@ public:
 	MyVector(const MyVector&);
 	MyVector& operator= (const MyVector&);
 	bool operator!= (const MyVector&) const;
+	int operator[] () const;
 	unsigned int size();
 	unsigned int capacity();
 private:
