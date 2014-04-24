48ad53da7a9a64125c25794d2f933cd77e81b145
darthvade <darthvade@sina.com>
2014-04-24 22:10:01 +0800

    2014-04-24 22:10:01 git-auto-saved

--- Week_8/4-23/Threadpool/Condition.cpp
+++ Week_8/4-23/Threadpool/Condition.cpp
@@ -11,3 +11,12 @@ Condition::~Condition() {
 void Condition::wait() {
 	pthread_cond_wait(&_cond, &_pMutex->_mutex);
 }
+
+void Condition::notify() {
+	pthread_cond_signal(&_cond);
+}
+
+void Condition::notifyall() {
+	pthread_cond_broadcast(&_cond);
+}
+
