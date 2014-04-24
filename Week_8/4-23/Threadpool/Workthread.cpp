3eb791d9d8bb8ec8747266eb6c6de929029fc878
darthvade <darthvade@sina.com>
2014-04-24 22:15:01 +0800

    2014-04-24 22:15:01 git-auto-saved

--- Week_8/4-23/Threadpool/Workthread.cpp
+++ Week_8/4-23/Threadpool/Workthread.cpp
@@ -22,6 +22,7 @@ void Workthread::register_threadpool(Threadpool *pThreadpool) {
 
 void Workthread::compute_task(int num) {
 	cout << "I'm thread --->" << pthread_self() << endl;
-	cout << "num: " << endl;
+	cout << pthread_self() << "strat ! " << endl;
 	sleep(5);
+	cout << "end ! " << endl;
 }
