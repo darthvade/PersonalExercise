f08c1c4422f1c0b9ca368b2bf08a0bc4af590cc7
darthvade <darthvade@sina.com>
2014-04-24 17:04:01 +0800

    2014-04-24 17:04:01 git-auto-saved

--- Week_8/4-24/cpp/producer_consumer.cpp
+++ Week_8/4-24/cpp/producer_consumer.cpp
@@ -6,10 +6,23 @@ pthread_mutex_t mutex;
 pthread_cond_t empty;
 pthread_cond_t full;
 
+
+void *consumer(void *) {
+
+	pthread_exit(0);
+}
+
+void *producer(void *) {
+
+	pthread_exit(0);
+}
+
+
 int main() {
 	pthread_mutex_init(&mutex, NULL);
 	pthread_cond_init(&empty, NULL);
 	pthread_cond_init(&full, NULL);
+
 
 
 	return 0;
