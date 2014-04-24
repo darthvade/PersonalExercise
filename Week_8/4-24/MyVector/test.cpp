1d6c3562297bb74bc6adc78d4a7b96b73fe1e24d
darthvade <darthvade@sina.com>
2014-04-24 13:14:01 +0800

    2014-04-24 13:14:01 git-auto-saved

--- Week_8/4-24/MyVector/test.cpp
+++ Week_8/4-24/MyVector/test.cpp
@@ -10,6 +10,8 @@ int main() {
 	cout << vec.capacity() << endl;
 	*/
 	MyVector vec1;
-	MyVector vec2;
+	MyVector vec2(30);
+	int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	MyVector vec3(a, a + 5);
 	return 0;
 }
