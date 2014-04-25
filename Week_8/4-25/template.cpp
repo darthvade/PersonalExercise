b0d2e975bd5beece7ee219bab680d349e16807e5
darthvade <darthvade@sina.com>
2014-04-25 22:36:01 +0800

    2014-04-25 22:36:01 git-auto-saved

--- Week_8/4-25/template.cpp
+++ Week_8/4-25/template.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 template <typename T>
-bool complare(const T &t1, const T &t2) {
+bool compare(const T &t1, const T &t2) {
 	return t1 > t2;
 }
 
 int main() {
-
+	compare(string("hehe"), string("bbb"));
 	return 0;
 }
